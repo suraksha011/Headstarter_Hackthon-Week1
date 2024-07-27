375cb0653313d3e7ff9cf721e8d0907f76b91e95
rozer <shubhamsinha07084@gmail.com>
2024-07-27 23:40:31 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Headstarter Hackathon Week - 1</h1>
+      <h1 className="text-2xl text-red-100 bg-red-400 p-4">
+        Headstarter Hackathon Week - 1
+      </h1>
     </>
   );
 }
