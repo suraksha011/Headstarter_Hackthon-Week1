ae685cd7635c8a730dc96636573441d80cc422c6
rozer <shubhamsinha07084@gmail.com>
2024-07-28 13:09:50 +0530

    landing page ✅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      bgColor: "#fff",
-      btnColor: "#6651cd",
-      bgChatBox: "#F2EDED",
-      chatTextColor: "#000",
-    },
-    fontSize: {
-      fontHeading: "30px",
-      fontBody: "24px",
-      fontText: "20px",
-    },
     extend: {},
   },
   plugins: [],
